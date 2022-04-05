5eea413067eccfebb1077819481123ef1b0d952d
Bruno Martins <bgcngm@gmail.com>
2022-04-05 10:34:38 +0100

    sm8350-common: Inherit from QTI FCM
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,7 +58,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(COMMON_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/device_framework_matrix.xml
 
