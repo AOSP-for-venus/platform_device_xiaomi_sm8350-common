2a620045d672f9418b1aef88219d71ac41981f62
Jake Weinstein <jake@aospa.co>
2022-08-09 07:13:07 +0900

    sm8350-common: rootdir: Do not allow restricted tasks to run on big cores
    The restricted cpuset is for system tasks that are
    throttled because the screen is off. Google only
    runs these tasks on the little cluster
    to save power and we will follow suit.
    
    Change-Id: I0174beb11dae2109df4ddf65304bd50c5cc05264
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- rootdir/bin/init.kernel.post_boot-shima.sh
+++ rootdir/bin/init.kernel.post_boot-shima.sh
@@ -181,6 +181,7 @@ echo 0 > /proc/sys/kernel/sched_coloc_busy_hysteresis_enable_cpus
 
 # cpuset parameters
 echo 0-3 > /dev/cpuset/background/cpus
+echo 0-3 > /dev/cpuset/restricted/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
 
 # configure governor settings for silver cluster
