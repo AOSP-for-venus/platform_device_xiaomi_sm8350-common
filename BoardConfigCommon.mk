bf3a1c7c54b465acba97900a95be4dd46bf8223b
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    Reland "sm8350-common: Switch to legacy sepolicy_vndr"
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -171,7 +171,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2022-08-01
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SELINUX_IGNORE_NEVERALLOWS := true
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
