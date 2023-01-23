a72023bdca133ec46e59e837dfb8f99c4f96d02f
Akash Srivastava <akashniki@gmail.com>
2023-01-23 19:42:44 +0800

    sm8350-common: Add Perf System blobs from LA.QSSI.13.0.r1-06400-qssi.0 * @markakash ported CLO's Frawework boost to AOSP, but for that to work properly these libs are needed
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- common.mk
+++ common.mk
@@ -417,6 +417,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_PACKAGES += \
+    libpsi.vendor \
+    libtflite
+
+# Perf Jars
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
+
 # Platform
 PRODUCT_BOARD_PLATFORM := lahaina
 PRODUCT_USES_QCOM_HARDWARE := true
@@ -508,6 +518,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
+
+# Thernal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
