638d97a20ad55dc4aa3973dcb484380c1cbfeda9
kondors1995 <normandija1945@gmail.com>
2023-01-28 16:04:58 +0800

    sm8350-common: Switch to source built IFAAService
    Change-Id: I093bb7299c4731026c2df06645cba1cc537c9e76
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- common.mk
+++ common.mk
@@ -267,6 +267,10 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder.vendor
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    IFAAService
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.batterysecret.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.batterysecret.rc \
