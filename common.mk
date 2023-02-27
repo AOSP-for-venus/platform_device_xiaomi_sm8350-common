e6f642a849bb935f1af2eaf68c89d6ec0681f0f9
ralph950412 <ralph950412@gmail.com>
2023-02-28 00:53:26 +0800

    Revert "sm8350-common: Declare support for blur"
    * Move to device/xiaomi/venus
    
    This reverts commit 968af531093e4608e66a0cd2f74f356cd6e191a0.
    
    Change-Id: I8f3cae72b00bdfb2bd34147a03a67635124dd70c
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- common.mk
+++ common.mk
@@ -134,9 +134,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
-# Blur
-TARGET_USES_BLUR := true
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
