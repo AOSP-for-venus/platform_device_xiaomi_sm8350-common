b3176920f5168d4682cf3afa41716996a4a5d676
Vaisakh Murali <mvaisakh@statixos.com>
2023-03-09 15:13:41 +0800

    sm8350-common: Append device framework matrices
    Change-Id: I7fcaf52baf02aef8d3cd9f651cf576bd2aa52f8d
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,7 +78,7 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/hidl/compatibility_matrix.xml
 
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/device_framework_matrix.xml
