090357d30ea090e3d4092ce630fd7f86fc6aa02e
ralph950412 <ralph950412@gmail.com>
2023-08-12 19:24:09 +0800

    sm8350-common: Don't build goodix fingerprint lib
    * Move to device-specific trees
    
    Change-Id: Icf42a2e37e8da5aa9796bdfc845281e255901596

--- common.mk
+++ common.mk
@@ -194,8 +194,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3.vendor \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
-    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
