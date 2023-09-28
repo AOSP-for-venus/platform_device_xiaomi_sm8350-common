d65513e97b709892237e7596abcc5da90d6e7d9c
ralph950412 <ralph950412@gmail.com>
2023-09-28 10:55:08 +0800

    sm8350-common: Use `task_profiles.json` from system/core
    Change-Id: I0ed4bfeba414bb68e0db29de75108cd84573db78

--- common.mk
+++ common.mk
@@ -428,7 +428,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.3.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Platform
 PRODUCT_BOARD_PLATFORM := lahaina
