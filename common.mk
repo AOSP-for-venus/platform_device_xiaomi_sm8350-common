551d019f13b326d80a85ae3dd675adca7e696f74
Carter Hsu <carterhsu@google.com>
2023-12-25 11:09:25 +0000

    [DNM] sm8350-common: Run audio hardware as 64bit
    Bug: 205240941
    Test: regression test is passed. (b/207432169)
    Signed-off-by: Carter Hsu <carterhsu@google.com>
    Change-Id: Ieb6ff80aa61108e18fed65ad02f9754f59a2812c

--- common.mk
+++ common.mk
@@ -30,6 +30,11 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Audio
+SOONG_CONFIG_NAMESPACES += android_hardware_audio
+SOONG_CONFIG_android_hardware_audio += \
+    run_64bit
+SOONG_CONFIG_android_hardware_audio_run_64bit := true
+
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
