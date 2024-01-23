7480efcf611a73f01c27fff5825b92e3eaad8d6b
ralph950412 <ralph950412@gmail.com>
2024-01-23 10:34:16 +0800

    TMP use more vendorCode(s)

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -94,10 +94,10 @@ class XiaomiUdfpsHander : public UdfpsHandler {
     }
 
     void onAcquired(int32_t result, int32_t vendorCode) {
-        if (result == FINGERPRINT_ACQUIRED_GOOD) {
+        if (result == FINGERPRINT_ACQUIRED_GOOD || vendorCode == 44 || vendorCode == 25) {
             set(FOD_HBM_PATH, FOD_HBM_OFF);
             set(FOD_STATUS_PATH, FOD_STATUS_OFF);
-        } else if (vendorCode == 21 || vendorCode == 23) {
+        } else if (vendorCode == 21 || vendorCode == 22 || vendorCode == 23) {
             /*
              * vendorCode = 21 waiting for fingerprint authentication
              * vendorCode = 23 waiting for fingerprint enroll
