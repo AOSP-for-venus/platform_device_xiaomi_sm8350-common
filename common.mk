936190c230884bbd9d4c975d9e58e8012df68cca
Arian <arian.kulmer@web.de>
2024-06-02 03:29:32 +0200

    sm8350-common: Add nonui notifier
    Change-Id: Id447dec0d6c6a384a55a25db45f2d763ba23635a
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- common.mk
+++ common.mk
@@ -516,6 +516,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 # Touchscreen
+PRODUCT_PACKAGES += \
+    nonui-notifier
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
