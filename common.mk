5a1670106fb7417d4b6ef9fbf6f1c5b60684a4a4
Vishalcj17 <vishalcj@aospa.co>
2024-06-14 23:52:51 +0800

    [TEST] sm8350-common: Add support for DeviceAsWebcam
    Change-Id: Iae52cdd12ac72fd6cde1be4a20c219a85555d67e
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- common.mk
+++ common.mk
@@ -387,6 +387,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    DeviceAsWebcamOverlayCommon \
     FrameworksResCommon \
     SettingsResCommon \
     SystemUIResCommon \
