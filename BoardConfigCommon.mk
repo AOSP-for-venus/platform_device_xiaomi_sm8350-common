69bac8fe911a8f4c223f9d754ef1fc4ea13c3c80
FlowerSea0208 <zzp060208@gmail.com>
2024-08-23 02:10:24 +0800

    sm8350-common: Switch to common lineage Powershare hal
    Change-Id: Iadddff9dbe9cf86e8510ddae165ddb04bcabe01d
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -139,10 +139,8 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 BOARD_VENDOR := xiaomi
 TARGET_BOARD_PLATFORM := lahaina
 
-# Powershare
-SOONG_CONFIG_NAMESPACES += XIAOMI_POWERSHARE
-SOONG_CONFIG_XIAOMI_POWERSHARE := WIRELESS_TX_ENABLE_PATH
-SOONG_CONFIG_XIAOMI_POWERSHARE_WIRELESS_TX_ENABLE_PATH := /sys/class/qcom-battery/reverse_chg_mode
+# PowerShare
+TARGET_POWERSHARE_PATH := /sys/class/qcom-battery/reverse_chg_mode
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
