b4bc43d2fd0f59620c9d4fa8d2c3a5be460c5d98
Yumi Yukimura <me.cafebabe@gmail.com>
2024-09-08 01:24:57 +0800

    sm8350-common: Use libcrypto-v33 for widevine libs
    * The symbol `CBS_init` is no longer exported from latest libcrypto.
    * libcrypto from VNDK 33 is available in hardware/statix/compat,
      and we had no problem with it on VNDK 33. Let's use it.
    
    Change-Id: I6e9b0857df4650e8d5d63793716fe84da93ab165
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -75,6 +75,9 @@ function blob_fixup() {
         vendor/lib64/c2.dolby.client.so)
             grep -q "dolbycodec_shim.so" "${2}" || "${PATCHELF}" --add-needed "dolbycodec_shim.so" "${2}"
             ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
+            ;;
     esac
 }
 
