cc97b50de393c6e40628b7f39e8162d88cdac934
Christopher Ferris <cferris@google.com>
2024-12-08 21:17:13 +0800

    [DNM] sm8350-common: Enable 32 bit building, but only 64 bit zygote.
    Bug: 229787331
    Test: Build/Boot in progress.
    Change-Id: I4686b567fd96a6cd7c3f6e269f0424811c0b3d6a
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,6 +29,14 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
 
+# Enable 64-bit for non-zygote.
+ZYGOTE_FORCE_64 := true
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a76
+
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
