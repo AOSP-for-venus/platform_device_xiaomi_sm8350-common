8ddc58ee8d919f55a5bedb458cd88e490c0860a2
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    sm8350-common: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- common.mk
+++ common.mk
@@ -353,6 +353,12 @@ PRODUCT_PACKAGES += \
     WifiResCommon
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint \
+    vendor_vm-system_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Platform
