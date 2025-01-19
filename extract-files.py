439ae675839bcb288303a7892c5e8b4c6a152826
Nick Reuter <nreuter85@gmail.com>
2025-01-19 23:52:51 +0800

    sm8350-common: Address missing libbinder symbols in libwfdnative
    _ZN7android6binder12atrace_beginEmPKc
    _ZN7android6binder10atrace_endEm
    
    Change-Id: I942d51721461dbbf9c8d0f2f32128529bb7480c3

--- extract-files.py
+++ extract-files.py
@@ -55,6 +55,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so')
+        .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
     'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
         .regex_replace('.*ozoaudio.*\n?', ''),
