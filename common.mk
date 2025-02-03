62e02ac723d501beb42f4bc6ee039de444b0855d
ralph950412 <ralph950412@gmail.com>
2025-02-03 22:46:11 +0800

    [DNM] sm8350-common: Build TurboAdapter
    Change-Id: I4f4c8cb700e38f4c65c33d72eaa3301abb6be622

--- common.mk
+++ common.mk
@@ -392,6 +392,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_PACKAGES += \
+    TurboAdapter
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
