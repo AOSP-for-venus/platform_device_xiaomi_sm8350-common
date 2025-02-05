fc2d264b7ea3b21b89d16f6369e44df84ee74b9e
Dhina17 <dhinalogu@gmail.com>
2025-02-06 03:00:25 +0800

    sm8350-common: Implement torch light control
    [RealJohnGalt: Modify for oneplus usage and add toggle switch]
    [cyberknight777/ralph950412: Adapt nodes and SEPolicy for Xiaomi SM8350]
    
    Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,6 +42,9 @@ TARGET_2ND_CPU_VARIANT := cortex-a76
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
+# Camera
+TARGET_CAMERA_SERVICE_EXT_LIB := //$(COMMON_PATH):libcameraservice_extension.xiaomi_sm8350
+
 # Dolby Vision
 SOONG_CONFIG_NAMESPACES += dolby_vision
 SOONG_CONFIG_dolby_vision += enabled
