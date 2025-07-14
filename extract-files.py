a1017f3aad654df21e4b696b662fa1209357a47a
ralph950412 <ralph950412@gmail.com>
2025-07-14 12:17:26 +0800

    sm8350-common: Shim c2.dolby.client with libcodec2_shim instead
    Change-Id: I812dd4c93522f3b43568d1c49cb1ffbf42958f0a

--- extract-files.py
+++ extract-files.py
@@ -60,6 +60,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('.*ozoaudio.*\n?', ''),
     'vendor/lib64/android.hardware.secure_element@1.0-impl.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
+    'vendor/lib64/c2.dolby.client.so': blob_fixup()
+        .add_needed('libcodec2_shim.so'),
     ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
 }  # fmt: skip
