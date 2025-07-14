c8c505c940674a62ccbdcd74b06cdc6f0e1f6fcc
ralph950412 <ralph950412@gmail.com>
2025-07-14 12:16:39 +0800

    Revert "sm8350-common: Add dolbycodec_shim"
    This reverts commit 7350956484073a859520b9e9e472c465636fe19b.
    
    Change-Id: I24b621ed6ebcb8187643e41a067d83df01178721

--- common.mk
+++ common.mk
@@ -179,8 +179,7 @@ PRODUCT_COPY_FILES += \
 # Codec2
 PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
-    libcodec2_hidl@1.0.vendor \
-    dolbycodec_shim
+    libcodec2_hidl@1.0.vendor
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
