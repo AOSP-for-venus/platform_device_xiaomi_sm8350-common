be0b941a450bbf6a16d0a310ba32e72a148dd767
kyasu <kyasu.dev@gmail.com>
2025-07-30 23:06:15 +0900

    sm8350-common: Disable `thumbnail_block_model` of libstagefright
    Change-Id: I86f92335af282d3de508150bbbcb3026993ec48c

--- common.mk
+++ common.mk
@@ -328,6 +328,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/yupik/media_codecs_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v1.xml \
     $(LOCAL_PATH)/media/yupik/media_profiles_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_yupik_v1.xml
 
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+
 # Minijail
 PRODUCT_PACKAGES += \
     libavservices_minijail_vendor
