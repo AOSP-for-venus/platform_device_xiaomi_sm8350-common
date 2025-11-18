b81176249cd37d13bc5cf8433b9124218a1c0968
Omkar Chandorkar <gotenksIN@aospa.co>
2025-11-18 18:55:39 +0800

    sm8350-common: Enable Wifi overlay for mainline modules
    Change-Id: Iac8be06e58d24d868a33d212d80a583bad8705ef
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- common.mk
+++ common.mk
@@ -368,6 +368,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     DeviceAsWebcamOverlayCommon \
     FrameworksResCommon \
+    GoogleWifiResCommon \
     SettingsResCommon \
     SystemUIResCommon \
     TelephonyResCommon \
