dbf5cf583d7e68bd18bbda4b78d639ce207af91e
ralph950412 <ralph950412@gmail.com>
2026-06-29 19:46:12 +0800

    permissive
    Change-Id: Ic920b0338c27c4863df1069068434d96c323057f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,6 +89,7 @@ BOARD_KERNEL_CMDLINE += pcie_ports=compat
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
